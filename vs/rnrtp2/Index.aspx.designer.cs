24cbb961acc4ae34c932c997dd91bfcb6e8be46a
Ziyanmaknojia <ziyanmak@gmail.com>
2022-04-12 23:46:47 -0500

    front changes

--- vs/rnrtp2/Index.aspx.designer.cs
+++ vs/rnrtp2/Index.aspx.designer.cs
@@ -24,6 +24,141 @@ namespace rnrtp2
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// HyperLink16 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink16;
+
+        /// <summary>
+        /// HyperLink17 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink17;
+
+        /// <summary>
+        /// HyperLink18 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink18;
+
+        /// <summary>
+        /// HyperLink19 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink19;
+
+        /// <summary>
+        /// HyperLink20 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink20;
+
+        /// <summary>
+        /// HyperLink21 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink21;
+
+        /// <summary>
+        /// HyperLink22 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink22;
+
+        /// <summary>
+        /// HyperLink23 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink23;
+
+        /// <summary>
+        /// HyperLink24 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink24;
+
+        /// <summary>
+        /// HyperLink25 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink25;
+
+        /// <summary>
+        /// HyperLink26 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink26;
+
+        /// <summary>
+        /// HyperLink27 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink27;
+
+        /// <summary>
+        /// HyperLink28 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink28;
+
+        /// <summary>
+        /// HyperLink29 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink29;
+
+        /// <summary>
+        /// HyperLink30 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink30;
+
+        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
@@ -121,5 +256,41 @@ namespace rnrtp2
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink HyperLink9;
+
+        /// <summary>
+        /// HyperLink12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink12;
+
+        /// <summary>
+        /// HyperLink13 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink13;
+
+        /// <summary>
+        /// HyperLink14 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink14;
+
+        /// <summary>
+        /// HyperLink15 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink15;
     }
 }
--- vs/rnrtp2/SearchVisitor.aspx
+++ vs/rnrtp2/SearchVisitor.aspx
@@ -4,24 +4,27 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+    <link rel="stylesheet" href="CCSMAIN.css"/>
     <title></title>
 </head>
-<body>
+<body id ="SearchVisitor">
     <form id="form1" runat="server">
         <div>
-            <h2>visitor information</h2>
-            <asp:Label ID="Label1" runat="server" Text="Ticket ID: "></asp:Label>
-            <asp:TextBox ID="id_textbox" runat="server"></asp:TextBox><br /><br />
-            <asp:Label ID="Label2" runat="server" Text="Visit Date: "></asp:Label>
-            <asp:TextBox ID="date_textbox" runat="server"></asp:TextBox><br /><br />
+            <fieldset>
+                <h2>visitor information</h2>
+                <asp:Label ID="Label1" runat="server" Text="Ticket ID: "></asp:Label>
+                <asp:TextBox ID="id_textbox" runat="server"></asp:TextBox><br /><br />
+                <asp:Label ID="Label2" runat="server" Text="Visit Date: "></asp:Label>
+                <asp:TextBox ID="date_textbox" runat="server"></asp:TextBox><br /><br />
 
-            <h2>update information</h2>
-            <asp:Label ID="Label3" runat="server" Text="Ticket Type: "></asp:Label>
-            <asp:TextBox ID="type_textbox" runat="server"></asp:TextBox><br /><br />
-            <asp:Label ID="Label4" runat="server" Text="Email: "></asp:Label>
-            <asp:TextBox ID="email_textbox" runat="server"></asp:TextBox><br /><br />
+                <h2>update information</h2>
+                <asp:Label ID="Label3" runat="server" Text="Ticket Type: "></asp:Label>
+                <asp:TextBox ID="type_textbox" runat="server"></asp:TextBox><br /><br />
+                <asp:Label ID="Label4" runat="server" Text="Email: "></asp:Label>
+                <asp:TextBox ID="email_textbox" runat="server"></asp:TextBox><br /><br />
 
-            <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click" />
+                <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click" />
+            </fieldset>
         </div>
     </form>
 </body>
