c5affb08f6eebf0694a828761cf9e3efc7bdc0bb
aigarci6 <itsai500@gmail.com>
2022-04-21 22:16:11 -0500

    Update AddClose.aspx.cs

--- vs/rnrtp2/AddClose.aspx.cs
+++ vs/rnrtp2/AddClose.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -51,6 +53,10 @@ namespace rnrtp2
         {
             MySqlConnection dbcon = new MySqlConnection("Server=rnrthemepark-db3380.mysql.database.azure.com; Port=3306; Database=theme_park; Uid=courtney@rnrthemepark-db3380; Pwd=cosc3380!; SslMode=Preferred;");
             MySqlCommand insert = new MySqlCommand("INSERT INTO closes (rideID, employeeID, date, time, type, cost, month, year) VALUES (@rideid, @empID, @dte, @tme, @tpe, @cst, @munth, @yeer);", dbcon);
+            MySqlCommand count = new MySqlCommand("SELECT COUNT(DISTINCT email) FROM staff;", dbcon);
+            MySqlCommand emails = new MySqlCommand("SELECT DISTINCT IFNULL(email, 0) FROM staff;", dbcon);
+            MySqlCommand delEmail = new MySqlCommand("DELETE FROM emailq WHERE idEmail=@emailID;", dbcon);
+            MySqlCommand emailQ = new MySqlCommand("SELECT IFNULL(idEmail, 0), IFNULL(subject, 0) FROM emailq, closes WHERE emailq.relatedID=closes.closeID AND closes.rideID=@ridID AND closes.employeeID=@stafID", dbcon);
 
             //parse date
             string str = date.Value;
@@ -64,12 +70,74 @@ namespace rnrtp2
             insert.Parameters.AddWithValue("@cst", cost.Text);
             insert.Parameters.AddWithValue("@yeer", dates[0]);
             insert.Parameters.AddWithValue("@munth", dates[1]);
-
-
+            emailQ.Parameters.AddWithValue("@ridID", rid_textbox.Text);
+            emailQ.Parameters.AddWithValue("@stafID", eid_textbox.Text);
 
             dbcon.Open();
-            insert.ExecuteReader();
-            //insert.ExecuteNonQuery();
+            insert.ExecuteNonQuery();
+
+            //retriving emailq info
+            MySqlDataReader emailQReader = emailQ.ExecuteReader();
+            emailQReader.Read();
+            int emailID = emailQReader.GetInt32(0);
+            string subject = emailQReader.GetString(1);
+            emailQReader.Close();
+
+            //retrieving number of emails           
+            MySqlDataReader emailCounter = count.ExecuteReader();
+            emailCounter.Read();
+            int eCount = emailCounter.GetInt32(0);
+            emailCounter.Close();
+
+
+            //deleting email row cuz info retrieved already
+            delEmail.Parameters.AddWithValue("@emailID", emailID);
+            delEmail.ExecuteNonQuery();
+
+            //retriving emails
+            MySqlDataReader emailsReader = emails.ExecuteReader();
+            for (int i = 0; i < eCount; i++)
+            {
+                emailsReader.Read();
+                string email = emailsReader.GetString(0);//need to find a way to iterate through rows; right now its doing through columns
+
+                SmtpClient client = new SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential()
+                    {
+                        UserName = "rocknrollthemepark.mysql@gmail.com",
+                        Password = "cosc3380!"
+                    }
+                };
+                MailAddress fromEmail = new MailAddress("rocknrollthemepark.mysql@gmail.com", "DB 3380");
+                MailAddress toEmail = new MailAddress(email, "The Hippest");
+                MailMessage message = new MailMessage()
+                {
+                    From = fromEmail,
+                    Subject = subject,
+                    Body = "Ride #" + rid_textbox.Text + " has been closed on " + date.Value + " at " + time_textbox.Text,
+                };
+                message.To.Add(toEmail);
+
+
+                try
+                {
+                    client.Send(message);
+                    /*MessageBox.Show("Sent Successfully", "Done");*/
+                }
+                catch (Exception ex)
+                { /*MessageBox.Show(ex.Message, "error");*/
+                    //errormessage.Visible = true;
+                }
+            }
+
+            emailsReader.Close();
+
             dbcon.Close();
 
             if (IsPostBack)
